acbd96295e59d7707d53597850603562188ee2a1
Roger Chen <rogermchen.ubc@gmail.com>
2025-01-12 22:13:40 -0700

    updated for admin

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,57 @@
 from django.contrib import admin
 
+from app.models import (Location, Merchant,
+                        TransactionType, Transaction,
+                        TransactionCategory, Person, EventType, Event)
 
+
+@admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
     list_display = ('name', 'street_no', 'street_name', 'city', 'state', 'country')
-    search_fields = ('name', 'unit_no', 'building_no', 'street_no',
-                     'street_name', 'city', 'state', 'zip_code', 'country')
+    search_fields = ('name', 'unit_no', 'building_no', 'street_no', 'street_name',
+                     'city', 'state', 'zip_code', 'country')
+
+
+@admin.register(Merchant)
+class MerchantAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name', 'website', 'phone', 'email', 'location')
+
+
+@admin.register(TransactionType)
+class TransactionTypeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(TransactionCategory)
+class TransactionCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('transaction_time', 'amount', 'transaction_type',
+                    'merchant', 'category')
+    search_fields = ('transaction_time', 'amount',
+                     'merchant', 'category')
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'merchant', 'gender')
+    search_fields = ('first_name', 'last_name', 'merchant__name', 'gender')
+
+
+@admin.register(EventType)
+class EventTypeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('event_time', 'event_type', 'name', 'location')
+    search_fields = ('event_time', 'event_type', 'name', 'location__city',
+                     'location__state', 'location__country')
--- app/models.py
+++ app/models.py
@@ -14,10 +14,17 @@ class Location(models.Model):
     building_no = models.IntegerField(null=True, blank=True)
     street_no = models.CharField(max_length=255, null=True, blank=True)
     street_name = models.CharField(max_length=255, null=True, blank=True)
-    city = models.CharField(max_length=255)
-    state = models.CharField(max_length=255)
+    city = models.CharField(max_length=255, null=True, blank=True)
+    state = models.CharField(max_length=255, null=True, blank=True)
     zip_code = models.CharField(max_length=255, null=True, blank=True)
     country = models.CharField(max_length=255, null=True)
+
+    class Meta:
+        verbose_name_plural = "Location"
+
+    def __str__(self):
+        return (f'{self.street_no} {self.street_name} {self.city} '
+                f'{self.state} {self.country}')
 
 
 class Merchant(models.Model):
@@ -26,6 +33,9 @@ class Merchant(models.Model):
                                  on_delete=models.CASCADE,
                                  null=True,
                                  blank=True)
+    website = models.URLField(null=True, blank=True)
+    phone = models.CharField(max_length=255, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
 
     class Meta:
         constraints = [
@@ -33,14 +43,23 @@ class Merchant(models.Model):
                                     name='unique_merchant_name'),
         ]
 
+    def __str__(self):
+        return self.name
+
 
 class TransactionType(models.Model):
     name = models.CharField(max_length=255)
     is_income = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.name
+
 
 class TransactionCategory(models.Model):
     name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
 
 
 class Transaction(models.Model):
@@ -66,14 +85,24 @@ class Person(models.Model):
     gender = models.ForeignKey(Gender,
                                on_delete=models.PROTECT)
 
+    class Meta:
+        verbose_name_plural = "People"
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
 
 class EventType(models.Model):
     name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
 
 
 class Event(models.Model):
     event_type = models.ForeignKey(EventType,
                                    on_delete=models.PROTECT)
+    event_time = models.DateTimeField()
     location = models.ForeignKey(Location,
                                  on_delete=models.PROTECT)
     name = models.CharField(max_length=255)
