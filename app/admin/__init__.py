28ee36cfcd075e437d1eb5e1ba0467ba602efdcf
Roger Chen <rogermchen.ubc@gmail.com>
2025-04-12 23:28:24 -0600

    remove user section

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -1,3 +1,10 @@
 from .account_admin import *
 from .other_admin import *
 from .transaction_admin import *
+
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
+
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
