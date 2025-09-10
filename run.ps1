e593115094e4ed3797aa5cb988f1ad30baaa55d6
Roger Chen <rogermchen.ubc@gmail.com>
2025-09-09 20:59:38 -0600

    remove powershell script and update package

--- run.ps1
+++
@@ -1,6 +0,0 @@
-$fullPath = Get-Location
-$venvPath = Join-Path $fullPath ".venv/Scripts/python.exe"
-$managePath = Join-Path $fullPath "manage.py"
-$cmd = "runserver"
-$runserver = "$venvPath $managePath $cmd"
-Invoke-Expression $runserver
