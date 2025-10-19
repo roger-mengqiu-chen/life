308ae878d5eb3d54f3dca926ac07a1bd9063bb28
Roger Chen <rogermchen.ubc@gmail.com>
2025-10-19 15:46:09 -0600

    update admin

--- investment_journal/admin.py
+++ investment_journal/admin.py
@@ -11,19 +11,23 @@ from investment_journal.models import (
 @admin.register(Sector)
 class SectorAdmin(admin.ModelAdmin):
     list_display = ('name',)
+    ordering = ('name',)
 
 
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
     list_display = ('title', 'date', 'sector')
+    ordering = ('date', 'title')
 
 
 @admin.register(Stock)
 class StockAdmin(admin.ModelAdmin):
     list_display = ('symbol', 'total_qty', 'sector', 'total_market_value', 'earnings')
     list_filter = ('sector',)
+    ordering = ('symbol',)
 
 
 @admin.register(StockTransaction)
 class StockTransactionAdmin(admin.ModelAdmin):
     list_display = ('stock', 'qty', 'date', 'price', 'fear_level')
+    autocomplete_fields = ('stock',)
