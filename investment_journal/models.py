3c5b97d5f64bb3b6d02bbde34cfad00dd589a084
Roger Chen <rogermchen.ubc@gmail.com>
2025-10-19 14:55:42 -0600

    add investment journal

---
+++ investment_journal/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+
+class Sector(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Stock(models.Model):
+    symbol = models.CharField(max_length=50, unique=True)
+    sector = models.ForeignKey(Sector, on_delete=models.CASCADE)
+    current_price = models.DecimalField(max_digits=10, decimal_places=2)
+    total_qty = models.DecimalField(max_digits=20, decimal_places=2)
+    total_market_value = models.DecimalField(max_digits=20, decimal_places=2)
+    total_cost = models.DecimalField(default=0, max_digits=20, decimal_places=2)
+    earnings = models.DecimalField(default=0, max_digits=20, decimal_places=2)
+
+    def __str__(self):
+        return self.symbol
+
+
+class News(models.Model):
+    title = models.CharField(max_length=255)
+    date = models.DateField()
+    sector = models.ForeignKey(Sector, on_delete=models.CASCADE)
+    content = models.TextField()
+
+    def __str__(self):
+        return f"{self.date} - {self.title}"
+
+
+class StockTransaction(models.Model):
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    qty = models.DecimalField(max_digits=20, decimal_places=2)
+    date = models.DateField()
+    price = models.DecimalField(max_digits=20, decimal_places=2)
+    fear_level = models.IntegerField()
+    note = models.TextField()
+    news = models.ManyToManyField(News)
+
+    def __str__(self):
+        return f'{self.stock}: {self.qty} {self.date}'
--- life/settings.py
+++ life/settings.py
@@ -22,7 +22,8 @@ INSTALLED_APPS = [
     'import_export',
     'rest_framework',
     'rangefilter',
-    'mylife'
+    'mylife',
+    'investment_journal',
 ]
 
 MIDDLEWARE = [
