1e0af1079615d92119d22dd86536c9749aa0f484
Roger Chen <rogermchen.ubc@gmail.com>
2026-08-02 16:01:54 -0600

    update

--- life/urls.py
+++ life/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
     path('django_plotly_dash/', include('django_plotly_dash.urls')),
     path('mylife/', include('mylife.urls')),
     path('', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
