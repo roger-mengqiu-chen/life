3125476e1fe17571af7df1d90d23e4fd4b50888e
Roger Chen <rogermchen.ubc@gmail.com>
2026-08-21 20:28:48 -0600

    update

--- life/urls.py
+++ life/urls.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 
+
 urlpatterns = [
     path('django_plotly_dash/', include('django_plotly_dash.urls')),
     path('mylife/', include('mylife.urls')),
